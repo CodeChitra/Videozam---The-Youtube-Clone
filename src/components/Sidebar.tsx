6ccc0ea181e3c907e62fba63cce31bd7f799d4c8
CodeChitra <codechitra@gmail.com>
2024-01-12 10:23:42 +0530

    Fixed A Search Bug And Completed Design Of Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,45 +1,35 @@
 import { Link } from "react-router-dom";
-
+const MENU = [
+  { title: "", values: ["Home", "Shorts", "Subscription"] },
+  {
+    title: "You",
+    values: ["Your Channel", "History", "Your Videos", "Watch Later"],
+  },
+  {
+    title: "Subscriptions",
+    values: [
+      "FreeCodeCamp",
+      "Tanay Pratap",
+      "CodeWithHarry",
+      "Akshay Saini",
+      "Academind",
+      "RJ Raunak",
+    ],
+  },
+];
 const Sidebar = () => {
   return (
     <aside className="bg-black p-5 text-white border-2 flex-1 max-h-[90vh] min-h-[90vh] overflow-y-scroll overflow-x-hidden border-none rounded-sm">
-      <ul>
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <li>Shorts</li>
-        <li>Subscription</li>
-      </ul>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
-      <div>sidebar</div>
+      {MENU.map((menu) => {
+        return (
+          <ul className="flex flex-col gap-3 border-b-2 mt-3">
+            {menu.title && <h3 className="text-xl font-bold">{menu.title}</h3>}
+            {menu.values.map((item) => {
+              return <li>{item}</li>;
+            })}
+          </ul>
+        );
+      })}
     </aside>
   );
 };
